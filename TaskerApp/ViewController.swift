7bc629e5c1f5706a7d60a673aff646626de2c244
turgaykirkil <turgaykirkil@gmail.com>
2023-10-02 09:35:17 +0300

    Initial Commit

---
+++ TaskerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskerApp
+//
+//  Created by Turgay KIRKIL on 2.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
